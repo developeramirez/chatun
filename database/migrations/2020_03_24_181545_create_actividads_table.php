016a37b1b1c7a27078d6d1034ae09ed0522d2771
Allan <allanramirez.edutec@gmail.com>
2020-04-14 10:54:57 -0600

    agrego migracion para la creacion de tabla de actividades

---
+++ database/migrations/2020_03_24_181545_create_actividads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActividadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actividads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('picture')->default('default.png');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actividads');
+    }
+}
